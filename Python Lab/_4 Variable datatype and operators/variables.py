95530c49bb440696acc60fae3d3c8554e255926a
Tapabrata Chowdhury <104613620+tapabrata12@users.noreply.github.com>
2024-03-03 17:23:02 +0530

    Some changes in variables file

--- Python Lab/_4 Variable datatype and operators/variables.py
+++ Python Lab/_4 Variable datatype and operators/variables.py
@@ -14,3 +14,15 @@ _a = 42
 # 1a = 42 It will through an error
 # $ = 42 It will through an error
 # %g = 42 It will through an error
+
+"""
+In Python there is a concept of reference variable so when write _a = 42 and  my_variable = 42
+so actually in memory there made a place type, that obj and reference no: are made and when we input 
+same object so both are pointing to the same thing _ --> 42 <-- my_variable just reference no:
+are updated 1 to 2
+"""
+# We can also know the memory address of the variable via id() method
+print(id(_a))
+print(id(my_variable))
+
+# In this case both are pointing same object so memory address for variable are same
